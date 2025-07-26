16da0f09657100462f01d90729cdf3edf7737930
doc2digi <doc2digilabs@gmail.com>
2025-07-26 06:54:51 +0530

    change .env to .env.local

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,8 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Key:', supabaseKey)
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL or Key is not defined in environment variables')
 }
